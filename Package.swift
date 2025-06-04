3378395f1263f0094965b189f224ed88cd4e988e
Kapil Teotia <kapil.teotia@urbanft.com>
2025-06-04 17:19:58 +0530

    SPM Added from RC Contour SDK

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RCContourSDK",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "RCContourSDK",
+            targets: ["RCContourSDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RCContourSDK",
+            url: "https://github.com/Contour-Document-Imaging/rc-contours-example/releases/download/1.0.0-alpha.2/RC_Contour_SDK_XCFramework.zip",
+            checksum: "40f801b0dc62060e8df6f18951f1f3371b2dd0f6f93cc3829d2c8d018f6b8cc3"),
+    ]
+)
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# rc-contour-sdk-spm
-Swift Package for RC Contour SDK
+
+# Swift Package for RC Contour SDK`
+
+## Documentation
+https://docs.fttoolbox.com/contoursai/ios/installation
+
+## License
+https://docs.fttoolbox.com/contoursai/legal/license
