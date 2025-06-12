e21b791b732d2e36617b764ec339c3dc0112dceb
Kapil Teotia <kapil.teotia@urbanft.com>
2025-06-12 18:41:50 +0530

    Version updated from 1.0.0-alpha.5

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RCContourSDK",
-            url: "https://github.com/Contour-Document-Imaging/rc-contours-example/releases/download/1.0.0-alpha.2/RCContourSDK.xcframework_1.0.0-alpha.2.zip",
-            checksum: "40f801b0dc62060e8df6f18951f1f3371b2dd0f6f93cc3829d2c8d018f6b8cc3"),
+            url: "https://github.com/Contour-Document-Imaging/rc-contours-example/releases/download/1.0.0-alpha.5/RCContourSDK.xcframework_1.0.0-alpha.5.zip",
+            checksum: "0feab59f62f759f373eaaf1c8954b9fe613bbd6e85d001948ab062f4784a4ec2"),
     ]
 )
 
